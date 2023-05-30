681a1b841fd700bce60095200a2eff99599902e1
Gabriel Gomes <248287@dac.unicamp.br>
2023-05-29 22:21:55 -0300

    Terminando a atividade 02

--- lab1/lab01-1.s
+++ lab1/lab01-1.s
@@ -1,8 +1,20 @@
 main:
-  li ra, 23929
+  li ra, 34921
   mul s0, s7, a0
+  xor t1, t5, a5
+  beq s0, s7, label
 
-  addi t0, zero, 4   # escolhe a operacao de leitura de inteiro (4)
+  lw zero, 16(gp)
+
+  li s0, 239239
+
+  sw t2, 16(tp)
+
+  lui a4, 39293
+
+  call teste
+  slli a0, t6, 4
+  slli t0, zero, 4   # escolhe a operacao de leitura de inteiro (4)
   ecall
   add s0, zero, a0
   addi t0, zero, 4   # escolhe a operacao de leitura de inteiro (4)
@@ -26,4 +38,9 @@ main:
   srl a0, s1, s0
   addi t0, zero, 1
   ecall
+
+label:
+  ret
+
+teste:
   ret
